a8204beab2e09032f890fdd3e23a5308aa0733d5
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-12-19 10:08:30 -0500

    update before go to the next task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router} from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 function App() {
   return (
     <Router>
-      <div className="flex flex-col justify-between h-screen">
+      <div className='flex flex-col justify-between h-screen'>
         <Navbar />
       </div>
-      <Navbar />
       <main>Content</main>
     </Router>
   );
