b78631bad432bfbccc529b9010d2fb46fac4e63c
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-12-19 19:45:56 -0500

    Home, About, NotFound pages

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -15,10 +15,10 @@ function Navbar({ title }) {
 
         <div className="flex-1 px-2 mx-2">
           <div className="flex justify-end">
-            <Link to="/" className="btn btn-ghost btn-sm rounded-btn">
+            <Link to="/" className="btn btn-ghost btn-sm rounded-btn mx-2">
               Home
             </Link>
-            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
+            <Link to="/about" className="btn btn-ghost btn-sm rounded-btn ">
               About
             </Link>
           </div>
