7a4e944257a2b0d25417e02aef3eb3579f4697ae
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-01-10 22:04:27 -0500

    Cleaning Up Our Actions & Axios

--- src/contex/github/GithubActions.jsx
+++ src/contex/github/GithubActions.jsx
@@ -1,55 +1,28 @@
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+import axios from 'axios'
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
-//Get search results
+const github = axios.create({
+  baseURL: GITHUB_URL,
+  header: { Authorization: `token ${GITHUB_TOKEN}` },
+})
+
+// Get search results
 export const searchUsers = async (text) => {
   const params = new URLSearchParams({
     q: text,
-  });
-  const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-    header: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  });
-  const { items } = await response.json();
-  console.log(items);
+  })
 
-  //we don't need these two lines, because we use useReduser hook
-  // setUsers(data);
-  // setLoading(false);
-  return items;
-};
+  const response = await github.get(`/search/users?${params}`)
+  return response.data.items
+}
 
-//Get single user
-export const getUser = async (login) => {
-  const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-    header: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  });
+// Get user and repos
+export const getUserAndRepos = async (login) => {
+  const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`),
+  ])
 
-  if (response.status === 404) {
-    window.location = "/notfound";
-  } else {
-    const data = await response.json();
-    return data;
-  }
-};
-
-//Get user repos
-export const getUserRepos = async (login) => {
-  const params = new URLSearchParams({
-    sort: "created",
-    per_page: 10,
-  });
-
-  const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-    header: {
-      Authorization: `token ${GITHUB_TOKEN}`,
-    },
-  });
-  const data = await response.json();
-  console.log(data);
-
-  return data;
-};
+  return { user: user.data, repos: repos.data }
+}
